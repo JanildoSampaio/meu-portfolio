7801bda04ae5bbd9373a51ebfd715c145654a2cf
JanildoSampaio <janildosc@gmail.com>
2024-12-01 20:12:58 -0300

    routescorrecao

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
+import Projects from "./pages/Projects/index.jsx";
 
 const router = createBrowserRouter([
   {
